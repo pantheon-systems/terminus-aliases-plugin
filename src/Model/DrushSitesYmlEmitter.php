2238b7662497713738c14ef4766c2e18eacfbc0b
Helen Yan <helenyan@berkeley.edu>
2019-07-09 14:35:10 -0700

    fixed tests

--- src/Model/DrushSitesYmlEmitter.php
+++ src/Model/DrushSitesYmlEmitter.php
@@ -44,19 +44,20 @@ class DrushSitesYmlEmitter implements AliasEmitterInterface
         $drushYmlEditor = new DrushYmlEditor($this->base_dir);
         $drushConfig = $drushYmlEditor->getDrushConfig();
         if (!is_null($drushConfig['drush']['paths']['alias-path'])) {
-            $drushConfig['drush']['paths']['alias-path'] = array_filter($drushConfig['drush']['paths']['alias-path'], array($this, 'filterForPantheon'));
+            $drushConfig['drush']['paths']['alias-path'] = array_filter($drushConfig['drush']['paths']['alias-path'], array($this, 'filterForSites'));
         }
         if (!is_null($drushConfig['drush']['paths']['include'])) {
-            $drushConfig['drush']['paths']['include'] = array_filter($drushConfig['drush']['paths']['alias-path'], array($this, 'filterForPantheon'));
+            $drushConfig['drush']['paths']['include'] = array_filter($drushConfig['drush']['paths']['alias-path'], array($this, 'filterForSites'));
         }
+        $drushConfigFiltered['drush']['paths']['alias-path'][] = str_replace($this->home, '${env.home}', $this->home . '/.drush/sites');
         $drushConfigFiltered['drush']['paths']['alias-path'][] = str_replace($this->home, '${env.home}', $pantheon_sites_dir);
         $drushConfigFiltered['drush']['paths']['include'][] = '${env.home}/.drush/pantheon';
         $drushYmlEditor->writeDrushConfig($drushConfigFiltered);
     }
 
-    protected function filterForPantheon($line)
+    protected function filterForSites($line)
     {
-        if (strpos($line, 'pantheon')) {
+        if (strpos($line, 'sites')) {
             return false;
         }
         return true;
--- tests/unit/DrushRcEmitterTest.php
+++ tests/unit/DrushRcEmitterTest.php
@@ -37,8 +37,8 @@ class DrushRcEmitterTest extends TestCase
     public function testDrushrcEmitter($expectedPath, $rawAliasData, $withDbUrl)
     {
         $aliasCollection = Fixtures::aliasCollection($rawAliasData, $withDbUrl);
-        $base_dir = '/.drush';
-        $location = Fixtures::mktmpdir() . '/pantheon.aliases.drushrc.php';
+        $base_dir = Fixtures::mktmpdir() . '/.drush/';
+        $location = $base_dir . '/pantheon.aliases.drushrc.php';
 
         $emitter = new AliasesDrushRcEmitter($location, $base_dir);
         $emitter->write($aliasCollection);
