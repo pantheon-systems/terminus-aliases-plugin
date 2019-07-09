e7e22c9f02efceab177234e4c29b0c526c1fd33f
Helen Yan <helenyan@berkeley.edu>
2019-07-09 14:00:04 -0700

    small fix

--- src/Model/DrushRcEditor.php
+++ src/Model/DrushRcEditor.php
@@ -39,7 +39,7 @@ class DrushRCEditor
             $drushRCContents = file_get_contents($drushRCPath);
         } else {
             $drushRCContents = '<?php' . "\n";
-            $newFile = fopen(getDrushRCPath(), $drushRCContents);
+            $newFile = fopen($this->getDrushRCPath(), $drushRCContents);
         }
         $drushRCContents = explode("\n", $drushRCContents);
         return $drushRCContents;
