5101b6a2e7b8d1226aed30b04c87d65f2a95fd27
Helen Yan <helenyan@berkeley.edu>
2019-07-10 13:29:13 -0700

    more changes

--- src/Model/Template.php
+++ src/Model/Template.php
@@ -47,7 +47,7 @@ class Template
     public static function copy($copyfrom, $target_dir)
     {
         $path = static::path($copyfrom);
-        $copied = copy($path, $target_dir);
+        $copied = copy($path, $target_dir . basename($copyfrom));
         return $copied;
     }
 
